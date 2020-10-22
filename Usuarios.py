9b60651aa2180485c0b89e6979fe9d5fb4e5cc7b
SAÚL <1996saul04@gmail.com>
2020-10-21 23:29:00 -0600

    Avances del Login

---
+++ Usuarios.py
@@ -0,0 +1,60 @@
+
+class Usuarios:
+
+	def __init__(self,id,nombre,apellido,usuario,password):
+
+		self.id = id
+		self.nombre = nombre 
+		self.apellido = apellido
+		self.usuario = usuario
+		self.password = password
+
+	#MÉTODOS GET	
+	def getId(self):
+		return self.id
+
+	def getNombre(self):
+		return self.nombre	
+
+	def getApellido(self):
+		return self.apellido
+
+	def getUsuario(self):
+		return self.usuario	
+
+	def getPassword(self):
+		return self.password
+
+	#MÉTODOS SET
+	def setId(self, id):
+		self.id = id
+
+	def setNombre(self, nombre):
+		self.nombre = nombre
+
+	def setApellido(self, apellido):
+		self.apellido = apellido
+
+	def setUsuario(self, usuario):
+		self.usuario = usuario	
+
+	def setPassword(self, password):
+		self.password = password
+
+	def dump(self):
+	
+		return {
+
+			'id' : self.id,
+			'nombre': self.nombre + " " +self.apellido,
+			'usuario': self.usuario
+		}			
+
+
+
+	
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, jsonify
+from Usuarios import Usuarios
+
+listaUsuarios = []
+contador = 1
+
+#ADMINISTRADOR
+listaUsuarios.append(Usuarios(0,"Usuario","Maestro","admin","admin"))
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+
+	if request.method == 'POST':
+
+		nombre = request.form.get('usuario')
+		passw = request.form.get('pass')
+
+		for user in listaUsuarios:
+
+			if user.getUsuario() == nombre and user.getPassword() == passw:
+				return user.dump()
+
+		return "Usuario No Encontrado"	
+
+	if request.method == 'GET':
+
+		nombre = listaUsuarios[0].usuario
+		return "<h1> BIENVENIDO: "+ nombre + "</h1>"	
+
+@app.route('/registro',methods=['POST'])
+def registro():
+
+	if request.method == 'POST':
+
+		nombre = request.form.get('Nombre')
+		apellido = request.form.get('Apellido')
+		usuario = request.form.get('Usuario')
+		passw = request.form.get('Password')
+
+		for user in listaUsuarios:
+
+			contador_temporal = 0
+
+			if user.getUsuario() == usuario:
+
+				return "El Usuario ya Existe"
+
+			elif contador_temporal == contador:
+
+				listaUsuarios.append(Usuarios(contador, nombre, apellido, usuario, passw))
+				contador += 1
+
+				return user.dump()
+
+			else:
+				contador_temporal += 1
+
+@app.route('/recuperacion-contraseña', methods=['POST'])
+def recuperacion():	
+
+	if request.method == 'POST':
+
+		usuario = request.form.get('Usuario')
+
+		for user in listaUsuarios:
+
+			contador_temporal = 0
+
+			if user.getUsuario() == usuario:
+
+				return "El Usuario ya Existe"
+
+			elif contador_temporal == contador:
+
+				return user.dump()
+
+			else:
+				contador_temporal += 1
+				
+
+if __name__ == "__main__":
+	app.run(threaded=True, port=5000,debug=True)		
